58ca082643746cafb1d9a3a5ab1a9c6e6a604656
Emil <emil88pl@gmail.com>
2023-04-16 10:11:55 +0100

    coffee_py

--- Coffee.py
+++ Coffee.py
@@ -1,12 +1,14 @@
 """
-the same resoult
-grind if there is no beans // behave 
-water empty - catch fire - result
+- one function one thing Single-responsibility principle SRP
+- names for functions and variabels 
+- K.I.S.S keep it simple 
 """
+
 import time
 
+
 grinding_time_S = 2
-boiling_time_S = 5
+boiling_time_S = 3
 pour_time_S = 2 
 add_milk_S = 1
 
@@ -17,21 +19,20 @@ def grind():
   beans = beans.lower()
   if beans == "decaf":
     global price
-    price += 100
+    price += 100 #
     print("Grinding decaf beans...")
     time.sleep(grinding_time_S) 
   elif beans == "normal":
-    
-    price += 200
+    price += 200 #
     print("Grinding normal beans...")
     time.sleep(grinding_time_S) 
   else:
     print("Add some beans!")
 
 def boildWater():
-  water = input("Do you have water? Y/N: ")
+  water = input("Is it cattle ready on the stove full of water? Y/N: ")
   water = water.lower()
-  while water != "y":
+  while water != "y": #
     print("Add water!") 
     water = input("How about now? Y/N: ")
     water = water.lower()
@@ -42,13 +43,13 @@ def boildWater():
 def addMilk():
   milk = input("Do you want to add some milk? it is only 20p!: ")
   milk = milk.lower()
-  while milk != "y":
+  while milk != "y": #
     print("OK no milk")
     break
   else:
     print("Adding milk...")
     global price
-    price += 20
+    price += 20 #
     time.sleep(add_milk_S)
 
 def pourWaterOverGrounds():
@@ -67,10 +68,21 @@ def tea_machine():
   boildWater()
   pourWaterOverGrounds()
   addMilk()
-  print("Your tea is ready!", "£", price/100)
-
-machine_with = {"2":coffee_machine, "1":tea_machine}
-tea_coffe = input("Press 1 for tea(£1) or 2 for coffe(£2): ")
-machine_with[tea_coffe]()
+  final_price = price/100
+  print("Your tea is ready!", "£"+ str(final_price))
 
 
+
+while True:
+  machine_with = {"2":coffee_machine, "1":tea_machine}
+
+  tea_coffe = input("Press 1 for tea(£1) or 2 for coffe(£2): ")
+
+  machine_with[tea_coffe]()
+
+
+##
+## delete hardcoded code
+## private _let
+## add class
+## write tp file
